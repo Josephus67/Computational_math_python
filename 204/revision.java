779d73a83a446d58e4cf8364133f358927bfb038
Josephus67 <jbawah002@st.ug.edu.gh>
2024-08-01 13:52:50 -0700

    find the max value

---
+++ 204/revision.java
@@ -0,0 +1,16 @@
+
+public class revision {
+
+    public static void main(String[] args) {
+        int[] arr = {254, 2, 12, 3, 45, 87, 12, 89, 22};
+        int max = arr[0]; 
+
+        for (int i = 0; i < arr.length; i++) { 
+            if (arr[i] > max) {
+                max = arr[i]; 
+            }
+        }
+
+        System.out.println("The maximum number is: " + max);
+    }
+}
