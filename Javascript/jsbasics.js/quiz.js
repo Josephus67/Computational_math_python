8fc7db8c6c3fd422c1b5dced03f3d56422acb782
Josephus67 <jbawah002@st.ug.edu.gh>
2024-11-07 12:07:26 -0800

    tutorial challenge test

--- Javascript/jsbasics.js/quiz.js
+++ Javascript/jsbasics.js/quiz.js
@@ -32,3 +32,26 @@ const jane = new Person(
 console.log(jane.hobbies);
 console.log(john.sayHello());
 console.log(jane.sayHello());
+
+//Tutorial challenge
+
+const bookTitle = "welcome home";
+const bookAuthor = "Jane Austen";
+const bookISBN = "9780198518555";
+const bookPubYear = "1871-06-18";
+function Book(title,author,isbn,pubYear){
+    this.title = title;
+    this.author = author;
+    this.isbn = isbn;
+    this.pubYear = pubYear;
+    this.getSummary = function(){
+        return `${this.title} was written by ${this.author} in ${this.pubYear}`;
+    }
+}
+
+const result = new Book(bookTitle,bookAuthor,bookISBN,bookPubYear);
+console.log(result.getSummary());
+console.log(result.title);
+console.log(result.author);
+console.log(result.isbn);
+console.log(result.pubYear);
