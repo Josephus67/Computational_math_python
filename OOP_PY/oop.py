6bf0ba7f84a786c05c031eb50694e7614ee68fce
Josephus67 <jbawah002@st.ug.edu.gh>
2024-11-14 14:39:25 -0800

    there is a chief difference between book1.getPrice() and getPrice(book1)

--- OOP_PY/oop.py
+++ OOP_PY/oop.py
@@ -5,10 +5,15 @@ class Book:
       self.pages = pages
       self.price = price
 
-book1 =Book("Python","Guido van Rossum",300,"$4")
-book2 = Book("C Programming","Kernighan & Ritchie",250,"$3")
-book3 = Book("Java","James Gosling",400,"$5")
+book1 =Book("Python","Guido van Rossum",300,4)
+book2 = Book("C Programming","Kernighan & Ritchie",250,3)
+book3 = Book("Java","James Gosling",400,5)
 
 print(book1.title)
 print(book2.author)
 print(book3.pages)
+def getPrice(self):
+   return self.price
+print(getPrice(book1))
+print(getPrice(book3))
+#print(book1.getprice())
