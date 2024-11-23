9ed07b06d1f1729f46098903dc9748f773220b24
Josephus67 <jbawah002@st.ug.edu.gh>
2024-11-22 17:44:09 -0800

    generate all possible passwords

---
+++ tomitokko/cs50.py
@@ -0,0 +1,16 @@
+
+def password(x: int):
+    from string import digits
+    for i in digits:
+        for j in digits:
+            for k in digits:
+                for l in digits:
+                    for m in digits:
+                        for n in digits:
+                            for o in digits:
+                                for p in digits:
+                                    for q in digits:
+                                        for r in digits:
+                                            print(i + j + k + l + m + n + o + p + q + r)
+
+password(6)
--- tomitokko/password.py
+++ tomitokko/password.py
@@ -10,3 +10,18 @@ for i in range(number_of_passwords):
     for j in range(length_of_password):
         passwords += random.choice(my_list)
     print(passwords)
+
+
+# print all possible passwords
+
+def password(x: int):
+    from string import digits
+    for i in digits:
+        for j in digits:
+            for k in digits:
+                for l in digits:
+                    for m in digits:
+                        for n in digits:
+                            print(i + j + k + l + m + n)
+
+password(6)
