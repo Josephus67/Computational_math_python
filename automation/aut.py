1cb4c1155884dcd2c1ccd1bd33f80108cd1dcc7d
Josephus67 <jbawah002@st.ug.edu.gh>
2024-12-01 19:42:55 -0800

    writing input to files

--- automation/aut.py
+++ automation/aut.py
@@ -1,11 +1,12 @@
 inputFile = open('automation/inputFile.txt', 'r')
 #print(inputFile.read())
-#inputFile.close()
 
-for line in inputFile:
-    split_line = line.split()
-    if split_line[2] == 'P':
-        print(split_line)
+# for line in inputFile:
+#     split_line = line.split()
+#     if split_line[2] == 'P':
+#         print(split_line)
+
+
 
 gross = open('automation/groceries.txt', 'r')
 gross_csv = open('automation/groceries.csv', 'r')
@@ -14,3 +15,21 @@ print("********************************")
 print(gross.read())
 print("********************************")
 print(gross_csv.read())
+
+#pass files and writing files
+passFile = open('automation/passFile.txt', 'w')
+
+failFile = open('automation/failFile.txt', 'w')
+
+for line in inputFile:
+    line_split = line.split()
+    if line_split[2] == 'P':
+        passFile.write(line)
+    else:
+        failFile.write(line)
+
+inputFile.close()
+
+
+passFile.close()
+failFile.close()
